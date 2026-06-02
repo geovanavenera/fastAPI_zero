b5bcb402b20c774b5b6911edaf00379fd3b604d2
Geovana Venera <geovana.venera@gmail.com>
2026-06-02 15:36:40 -0300

    atualizando endpoints para usar com banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,9 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
-
-from fastapi_zero.app import app
+from fastapi_zero.schemas import UserPublic
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_root_deve_retornar_ok_e_ola_mundo(client):
 
     response = client.get("/")
 
@@ -14,8 +11,7 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     assert response.json() == {"message": "Olá Mundo!"}
 
 
-def test_create_user():
-    client = TestClient(app)
+def test_create_user(client):
 
     response = client.post(
         "/users/",
@@ -33,25 +29,63 @@ def test_create_user():
     }
 
 
-def test_read_users():
-    client = TestClient(app)
+def test_read_users(client):
 
     response = client.get("/users/")
     assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"users": []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get("/users/")
+    assert response.json() == {"users": [user_schema]}
+
+
+def test_update_user(client, user):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        "users": [
-            {
-                "username": "alice",
-                "email": "alice@example.com",
-                "id": 1,
-            }
-        ]
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": 1,
     }
 
 
-def test_delete_user():
-    client = TestClient(app)
-    response = client.delete("/users/1")
+def test_update_integrity_error(client, user):
+    # Criando um registro para "fausto"
+    client.post(
+        "/users",
+        json={
+            "username": "fausto",
+            "email": "fausto@example.com",
+            "password": "secret",
+        },
+    )
 
+    # Alterando o user.username das fixture para fausto
+    response_update = client.put(
+        f"/users/{user.id}",
+        json={
+            "username": "fausto",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        "detail": "Username or Email already exists"
+    }
+
+
+def test_delete_user(client, user):
+    response = client.delete("/users/1")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "User deleted"}
