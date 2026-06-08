0d07882935a9e1807ad4f8c02fb76b0a49b2dacc
Geovana Venera <geovana.venera@gmail.com>
2026-06-08 07:54:16 -0300

    "feat(auth): protege endpoints PUT e DELETE com autenticação"

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from sqlalchemy.pool import StaticPool
 from fastapi_zero.app import app
 from fastapi_zero.database import get_session
 from fastapi_zero.models import User, table_registry
+from fastapi_zero.security import get_password_hash
 
 
 @pytest.fixture
@@ -39,9 +40,25 @@ def client(session):
 
 @pytest.fixture
 def user(session):
-    user = User(username="Teste", email="teste@test.com", password="testtest")
+    password = "testtest"
+    user = User(
+        username="Teste",
+        email="teste@test.com",
+        password=get_password_hash(password),
+    )
     session.add(user)
     session.commit()
     session.refresh(user)
 
+    user.clean_password = password
+
     return user
+
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        "/token",
+        data={"username": user.email, "password": user.clean_password},
+    )
+    return response.json()["access_token"]
