f94458a4614df8f48e550ba6e7e5e8ec8813d613
Geovana Venera <geovana.venera@gmail.com>
2026-06-15 09:58:00 -0300

    feat(auth): implementa refresh token e testes de autorização

--- fastapi_zero/security.py
+++ fastapi_zero/security.py
@@ -4,7 +4,7 @@ from zoneinfo import ZoneInfo
 
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jwt import DecodeError, decode, encode
+from jwt import DecodeError, ExpiredSignatureError, decode, encode
 from pwdlib import PasswordHash
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -15,6 +15,9 @@ from fastapi_zero.settings import Settings
 
 settings = Settings()
 pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="auth/token", refreshUrl="auth/refresh"
+)
 
 
 def create_access_token(data: dict):
@@ -37,9 +40,6 @@ def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
-
-
 async def get_current_user(
     session: AsyncSession = Depends(get_session),
     token: str = Depends(oauth2_scheme),
@@ -60,6 +60,9 @@ async def get_current_user(
             raise credentials_exception
 
     except DecodeError:
+        raise credentials_exception
+
+    except ExpiredSignatureError:
         raise credentials_exception
 
     user = await session.scalar(
